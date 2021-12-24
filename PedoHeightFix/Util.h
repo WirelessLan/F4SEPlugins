8ea141abbefe5c61afda33de758677397ca0b03e
WirelessLan <ywk0716@gmail.com>
2021-12-24 16:27:21 +0900

    _

--- PedoHeightFix/Util.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-float GetPlayerScale();
-TESForm* GetFormFromIdentifier(const std::string& identifier);
-bool isActorFemale(Actor* actor);
-void SetActorRefScale(Actor* actor, float scale, bool isFemale);
