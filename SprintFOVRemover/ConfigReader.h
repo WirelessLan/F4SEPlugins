eab8e575edce8b6130d821899c9612a0d88178e0
WirelessLan <ywk0716@gmail.com>
2022-01-12 20:16:11 +0900

    Delete ConfigReader.h

--- SprintFOVRemover/ConfigReader.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-template<typename T>
-bool GetConfigValue(const char* section, const char* key, T* dataOut);
-template<>
-bool GetConfigValue(const char* section, const char* key, bool* dataOut);
-bool GetConfigValue(const char* section, const char* key, UInt32* dataOut);
