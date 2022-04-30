872c6a697b5fe66c32988db0325d2b898c2285c5
WirelessLan <ywk0716@gmail.com>
2022-05-01 01:28:16 +0900

    Delete SkeletonManager.cpp

--- CustomScaleFix/SkeletonManager.cpp
+++
@@ -1,9 +0,0 @@
-#include "Global.h"
-
-void ModifyScale(Actor* actor, bool isFirstPerson, float scale) {
-	NiNode* rootNode = actor->GetActorRootNode(isFirstPerson);
-	if (!rootNode)
-		return;
-
-	rootNode->m_localTransform.scale = scale;
-}
