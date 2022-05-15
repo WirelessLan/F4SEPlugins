a674395d0d5a76e2e2b98280a44f0b5b57be4944
WirelessLan <ywk0716@gmail.com>
2022-05-15 13:15:48 +0900

    Removed some unnecessary functions for AAF doppelganger

--- AAFDynamicPositioner/Utility.cpp
+++ AAFDynamicPositioner/Utility.cpp
@@ -53,53 +53,6 @@ namespace Utility {
 		return LookupFormByID(actualFormId);
 	}
 
-	bool IsPrewarNora(UInt32 formId, UInt32 baseFormId) {
-		// BaseForm의 ID가 일치하는 레퍼런스 NPC가 둘 이상 존재할 수 있으므로
-		// 바닐라 레퍼런스 NPC인지 확인하여 무시
-		return formId != 0xA7D38 && baseFormId == 0xA7D35;
-	}
-
-	bool IsAAFDoppelganger(UInt32 formId) {
-		static TESForm* aaf_doppForm = Utility::GetFormFromIdentifier("AAF.esm", 0x72E2);
-		if (!aaf_doppForm)
-			return false;
-
-		return formId == aaf_doppForm->formID;
-	}
-
-	bool IsAAFDoppelganger(Actor* actor) {
-		if (!actor || !actor->baseForm)
-			return false;
-
-		bool isFemale = false;
-		TESNPC* actorBase = DYNAMIC_CAST(actor->baseForm, TESForm, TESNPC);
-		if (actorBase)
-			isFemale = CALL_MEMBER_FN(actorBase, GetSex)() == 1 ? true : false;
-
-		if (isFemale) {
-			if (IsPrewarNora(actor->formID, actor->baseForm->formID) || IsAAFDoppelganger(actor->baseForm->formID))
-				return true;
-		}
-		else if (IsAAFDoppelganger(actor->baseForm->formID)) {
-			return true;
-		}
-
-		return false;
-	}
-
-	Actor* GetAAFDoppelganer() {
-		if (*g_player && (*g_player)->parentCell) {
-			for (UInt32 ii = 0; ii < (*g_player)->parentCell->objectList.count; ii++) {
-				TESObjectREFR* ref = (*g_player)->parentCell->objectList.entries[ii];
-				Actor* actorRef = DYNAMIC_CAST(ref, TESObjectREFR, Actor);
-				if (actorRef && IsAAFDoppelganger(actorRef))
-					return actorRef;
-			}
-		}
-
-		return nullptr;
-	}
-
 	void trim(std::string& s) {
 		s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
 			return !std::isspace(ch);
--- AAFDynamicPositioner/Utility.h
+++ AAFDynamicPositioner/Utility.h
@@ -10,7 +10,5 @@ namespace Utility {
 	float GetActualScale(TESObjectREFR* refr);
 	TESForm* GetFormFromIdentifier(const std::string& pluginName, const std::string& formIdStr);
 	TESForm* GetFormFromIdentifier(const std::string& pluginName, const UInt32 formId);
-	bool IsAAFDoppelganger(Actor* actor);
-	Actor* GetAAFDoppelganer();
 	void trim(std::string& s);
 }
